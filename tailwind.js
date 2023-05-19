c15dd450fdc5e822f8fe01039e8347e14794132b
Adelle Dantas <86019270+AdelleDantas@users.noreply.github.com>
2023-05-19 14:56:38 -0300

    Add files via upload

---
+++ tailwind.js
@@ -0,0 +1,17 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        clifford: '#da373d',
+        dark: '#1F1F20',
+        purple: '#B718EF',
+        gray: '#3B3B3C',
+      },
+      backgroundImage: {
+        'loginBgImg': "url('./img/background_login.jpeg')",
+      }
+    }
+  }
+}
+
+
