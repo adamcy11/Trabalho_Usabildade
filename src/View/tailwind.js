2fefd48891f915bf07ec8e6f779540b43616e747
Adelle Dantas <86019270+AdelleDantas@users.noreply.github.com>
2023-05-25 23:29:52 -0300

    Update tailwind.js

--- src/View/tailwind.js
+++ src/View/tailwind.js
@@ -5,17 +5,21 @@ tailwind.config = {
         clifford: '#da373d',
         dark: '#1F1F20',
         gray: '#2A303C',
+        gray2: '#3B3B3C',
+        lightgray: '#848486',
         purple: '#B718EF',
         purpledark: '#3B0350',
         bluedark: '#03124F',
       },
       backgroundImage: {
         'loginBgImg': "url('./img/background_login.jpeg')",
-      }
+      },
     }
   }
 }
 
 
-  
-  
+function toggleMenu(button) {
+  var menu = button.nextElementSibling;
+  menu.classList.toggle("hidden");
+}
